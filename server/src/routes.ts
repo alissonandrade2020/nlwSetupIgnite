29a75f597e47a2cb405f20625284441e959b73b2
alissonandrade2020 <alisson2d@hotmail.com>
2023-01-20 16:24:43 -0300

    Aplicação de Habits - Aula 04 Deploy - error summary

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -172,7 +172,7 @@ export async function appRoutes(app: FastifyInstance) {
           JOIN habits H 
           ON H.id = HWD.habit_id
           WHERE
-            HWD.week_day = cast(strftime('%w', D.date/1000.0, 'unixepoch') as int )
+            HWD.week_day = cast(extract('%w', D.date/1000.0, 'unixepoch') as int )
           AND H.created_at <= D.date
         ) as amount
         FROM days D
