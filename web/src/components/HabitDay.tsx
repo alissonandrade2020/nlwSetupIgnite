01f4948f36612bb6c2f66421620ab1b52585128c
Alisson de Andrade Araújo <alisson2d@hotmail.com>
2023-01-20 11:03:43 -0300

    500

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -42,7 +42,7 @@ export function HabitDay({ completed = 0, amount = 0, date }: HabitDayProps) {
             <Checkbox.Root
               className='flex items-center gap-3 group'
             >
-              <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-50'>
+              <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
                 <Checkbox.Indicator>
                   <Check size={20} className="text-white" />
                 </Checkbox.Indicator>
