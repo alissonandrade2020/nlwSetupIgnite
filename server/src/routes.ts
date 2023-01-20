326907d7fc4607e9205c65bacdcd699cd94ab9a2
alissonandrade2020 <alisson2d@hotmail.com>
2023-01-20 17:53:30 -0300

    Aplicação de Habits - Aula 04 Deploy - error summary

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -156,10 +156,47 @@ export async function appRoutes(app: FastifyInstance) {
 
   app.get("/summary", async () => {
     const summary = await prisma.$queryRaw`
-      SELECT 
-       *
-        FROM days 
-      `
+              SELECT 
+        D.id, 
+        D.date,
+        (
+          SELECT 
+            count(*) as float
+          FROM day_habits DH
+          WHERE DH.day_id = D.id
+        ) as completed,
+        (
+          SELECT
+            count(*) as float
+          FROM habit_week_days HDW
+          JOIN habits H
+            ON H.id = HDW.habit_id
+          WHERE
+H.created_at <= D.date
+        ) as amount
+      FROM days D
+        
+      /* SELECT 
+        D.id, 
+        D.date,
+        (
+          SELECT 
+            cast(count(*) as float)
+          FROM day_habits DH
+          WHERE DH.day_id = D.id
+        ) as completed,
+        (
+          SELECT
+            cast(count(*) as float)
+          FROM habit_week_days HDW
+          JOIN habits H
+            ON H.id = HDW.habit_id
+          WHERE
+            HDW.week_day = cast(strftime('%w', D.date/1000.0, 'unixepoch') as int)
+            AND H.created_at <= D.date
+        ) as amount
+      FROM days D */
+      `;
     return summary;
   });
 }
--- web/src/lib/axios.ts
+++ web/src/lib/axios.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const api = axios.create({
   // baseURL: 'http://localhost:3333',
-  baseURL: 'https://nlwsetupignite-production.up.railway.app/'
+  baseURL: 'https://nlwsetupignite-production.up.railway.app'
 })
