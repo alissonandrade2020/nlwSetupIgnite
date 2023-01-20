b405a38546ee7336127f3fdab4c383fa012e2489
alissonandrade2020 <alisson2d@hotmail.com>
2023-01-20 13:21:43 -0300

    Aplicação de Habits - Aula 04 - Deploy

--- mobile/src/lib/axios.ts
+++ mobile/src/lib/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: 'https://nlwsetupignite-production.up.railway.app/'
+  baseURL: 'https://nlwsetupignite-production.up.railway.app'
 })
